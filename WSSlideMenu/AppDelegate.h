56ed999137501d89b950421622782391478acc0b
WebsoftProfession <dotsquares@praveens-mac-mini.local>
2017-07-06 16:52:23 +0530

    Initial Commit

---
+++ WSSlideMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSSlideMenu
+//
+//  Created by Dotsquares on 7/6/17.
+//  Copyright © 2017 WebsoftProfession. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WSSlideMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WSSlideMenu
+//
+//  Created by Dotsquares on 7/6/17.
+//  Copyright © 2017 WebsoftProfession. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
